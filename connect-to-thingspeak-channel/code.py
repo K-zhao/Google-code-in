e7730bbee755ee2aa25bf0e6bb070f4f46cc9a9b
kevinzhao <kevzhao2@gmail.com>
2018-11-25 00:09:15 -0800

    Pulls and reads Temperature and Humidity Values from Thingspeak

--- connect-to-thingspeak-channel/code.py
+++ connect-to-thingspeak-channel/code.py
@@ -1,9 +1,10 @@
+from urllib.request import urlopen
 import RPi.GPIO as GPIO
 import dht11
 import time
 import datetime
 import urllib2
-
+import json
 
 # initialize GPIO
 GPIO.setwarnings(False)
@@ -16,15 +17,24 @@ instance = dht11.DHT11(pin=14)
 
 
 while True:
-    result = instance.read()
-    Temperature = result.temperature
-    Humidity = result.humidity
+    sensor = instance.read()
+    Temperature = sensor.temperature
+    Humidity = sensor.humidity
     # Setting the API Key while inputing Temperature and Humidity Values to Thingspeak
     baseURL = "http://api.thingspeak.com/update?api_key=AP8KDTTOGBDP69ES&field1=%d&field2=%d" % (Temperature, Humidity)
     f = urllib2.urlopen(baseURL)
     f.read()
     f.close()
-    # Checking whether the sensor is actually giving correct values
-    print(Temperature)
-    print(Humidity)
+    time.sleep(2)
+    # Grabs data from Thingspeak
+    temperatureURL = urlopen("https://api.thingspeak.com/channels/630475/fields/1/last.json").read()
+    humidityURL = urlopen("https://api.thingspeak.com/channels/630475/fields/2/last.json").read()
+    result_temp = json.loads(temperatureURL)
+    result_hmd = json.loads(humidityURL)
+    # Pulls date and time when the data was created
+    dateandtime = result_temp['created_at']
+    date = dateandtime[0:10]
+    time = dateandtime[11:19]
+    # Prints when data was created, as well as Temperature and Humidity
+    print("Latest data from Thingspeak:", date, time, "Temperature:", result_temp["field1"], "C", "Humidity:", result_hmd["field2"], "%")
     time.sleep(2)
