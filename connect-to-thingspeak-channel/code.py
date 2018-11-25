c3ecd13febb1950b5e3074a97a23a749cc84745d
kevinzhao <kevzhao2@gmail.com>
2018-11-24 22:05:17 -0800

    Added comments

--- connect-to-thingspeak-channel/code.py
+++ connect-to-thingspeak-channel/code.py
@@ -19,10 +19,12 @@ while True:
     result = instance.read()
     Temperature = result.temperature
     Humidity = result.humidity
+    # Setting the API Key while inputing Temperature and Humidity Values to Thingspeak
     baseURL = "http://api.thingspeak.com/update?api_key=AP8KDTTOGBDP69ES&field1=%d&field2=%d" % (Temperature, Humidity)
     f = urllib2.urlopen(baseURL)
     f.read()
     f.close()
+    # Checking whether the sensor is actually giving correct values
     print(Temperature)
     print(Humidity)
     time.sleep(2)
